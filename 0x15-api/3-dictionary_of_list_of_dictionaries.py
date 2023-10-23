b4187fb2927d57c5ecce17012e9508b26f35fc87
valariembithe <valmbithe691@gmail.com>
2023-10-23 05:27:58 -0700

    Tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Records all tasks from all employees in json format """
 
-import requests
 import json
+import requests
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
