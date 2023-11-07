3709e32738296e32b6bc7cdd28abb025fb3f5cf1
valariembithe <valmbithe691@gmail.com>
2023-11-07 02:30:09 -0800

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" queries the Reddit API and returns the number of subscribers """
+import requests
+
+def number_of_subscribers(subreddit):
+    """ Returns number of subscribers"""
+    reddit_url = "https://www.reddit.com/r/{}/about.json"
+    url = reddit_url.format(subreddit)
+    response = requests.get(
+            url, headers={"User-Agent": "MyBot/1.0"}, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    data = response.json()
+    return data['data']['subscribers']
