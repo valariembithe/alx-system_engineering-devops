38015d53dcecfe124b8eb25ba493ea81ab7ed40b
valariembithe <valmbithe691@gmail.com>
2023-11-07 02:44:47 -0800

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
-""" 
+"""  queries the Reddit API and prints the titles 
+    of the first 10 hot posts listed 
+"""
 import requests
 
+def top_ten(subreddit):
+    """ prints the titles pf first 10 for a subreddit """
+    reddit_url = 'https://www.reddit.com/r/{}/hot.json?limit=10'
+    url = reddit_url.format(subreddit)
+    headers = {"User-Agent": "MyBot/1.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print (None)
+        return
+    data = response.json()
+    posts = data['data']['children']
+
+    for post in posts:
+        print(post['data']['title'])
